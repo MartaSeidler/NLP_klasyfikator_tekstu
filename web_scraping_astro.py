045fdb5c32604ec12e10aaab09541cd77bb070d6
Marta Seidler <124307153+MartaSeidler@users.noreply.github.com>
2024-06-23 00:20:34 +0200

    Update scraper

--- web_scraping_astro.py
+++ web_scraping_astro.py
@@ -1,28 +1,53 @@
 import requests
 import pandas as pd
+import time
 from bs4 import BeautifulSoup
 
 def scrap_article(article_url):
     response = requests.get(article_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    articles = soup.find_all('article')
+    content_div = soup.find('div', class_='post-content')
+    if content_div:
+        return content_div.get_text()
+    else:
+        return ''
 
-    texts = []
-    for article in articles:
-        title = article.find('h1', class_='post-title').text
-        content = ""
-        paragraphs = article.find_all('p', class_='text-align-justify')
-        for paragraph in paragraphs:
-            content += paragraph.text
-        texts.append(f"{title}\n{content}")
+def scrap_page(page_url, main_page_url):
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    return texts
+    article_links = soup.find_all('a', rel='bookmark')
 
+    links = []
+    for link in article_links:
+        article_url = link['href']
+        if not article_url.startswith('https'):
+            article_url = main_page_url + article_url
+        links.append(article_url)
 
-url = 'https://www.urania.edu.pl/wiadomosci/pokaz-mozliwosci-wielkiego-teleskopu-lornetkowego'
-texts = scrap_article(url)
+    return links
 
-data = {'text': texts, 'label': 'astronomia'}
+def scrap_all_pages(base_url, main_page_url, max_pages=5):
+
+    articles = []
+
+    for page in range(0, max_pages):
+        page_url = f"{base_url}?page={page}"
+        article_links = scrap_page(page_url, main_page_url)
+    
+        for link in article_links:
+            article_content = scrap_article(link)
+            articles.append(article_content)
+
+            time.sleep(1)
+
+    return articles
+
+urania_url = 'https://www.urania.edu.pl'
+urania_astronomia_url = 'https://www.urania.edu.pl/taxonomy/term/3096'
+articles_urania = scrap_all_pages(urania_astronomia_url, urania_url)
+
+data = {'text': articles_urania, 'label': 'astronomia'}
 df = pd.DataFrame(data)
 df.to_csv('articles_data.csv', index=False)
